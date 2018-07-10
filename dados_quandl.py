67abd9bd32516c89953d86676bcc8ae40da41c13
viniciuspantoja <vinicius.pantoja@gmail.com>
2018-07-09 22:11:51 -0400

    lista de tickers e atualização do codigo

--- dados_quandl.py
+++ dados_quandl.py
@@ -6,9 +6,15 @@ Created on Sat Jul  7 22:22:38 2018
 @author: ViniciusPantoja
 """
 #%%
+
+
 import good_morning as gm
 import pandas as pd
 import numpy as np
+import os
+
+os.chdir('/Users/ViniciusPantoja/Dropbox/Dividendos/Codigos')
+
 
 stock = 'AAPL'
 kr = gm.FinancialsDownloader()
@@ -43,7 +49,7 @@ dividend_ratio = div_ratio(kr_frames)
 
 #%%
 
-
+tickers = pd.read_csv('S&P_Ticker.csv')
 
 
 
